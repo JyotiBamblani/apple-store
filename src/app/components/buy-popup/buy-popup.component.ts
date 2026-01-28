55de0bef932a3bf92e4e3a0ecdf2431115cd61da
Jyoti <jbamblani07@gmail.com>
2026-01-28 12:57:19 +0530

    first commit

---
+++ src/app/components/buy-popup/buy-popup.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buy-popup',
+  imports: [],
+  templateUrl: './buy-popup.component.html',
+  styleUrl: './buy-popup.component.scss'
+})
+export class BuyPopupComponent {
+
+}
